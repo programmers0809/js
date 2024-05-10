9e7981d2ea405c506bb9b156e9b22173dd905e86
feruz abdullyev <165376645+programmers0809@users.noreply.github.com>
2024-05-10 16:36:02 +0500

    Add files via upload

---
+++ mani.js
@@ -0,0 +1,65 @@
+// malumot turlari
+// string
+// let names="tico"
+// let famliya="aliyev"
+// console.log(names);
+// int
+// let km=44;
+// let sm=300;
+// console.log(km)
+// console.log(sm)
+// let text="hello"
+// let text1="word"
+// console.log(text+text1);
+
+// console.log(text-text1);
+// console.log(text*text1);
+// console.log(text/text1);
+// 6
+// let num1=2;
+// let num2=4;
+// console.log(num1+num2);
+// console.log(num1-num2);
+// console.log(num1*num2);
+// console.log(num1/num2/);
+// ko'shi va ayrish1
+// let a=2;
+// let b="4";
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// ko'shi va ayrish2
+// let a=3;
+// let b="hi"
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// darja
+// let a=3
+// console.log(a**2);
+// console.log(b**3);
+// mASALA1
+// let a=3;
+// let p=4*a;
+// console.log(p);
+// m2
+// let s=1;
+// let a=5=a;
+// console.log(b);
+// m3\
+// let s=1*a*b
+// let p=2*a+b
+// console.log(p);
+// m4
+// let l=200
+// let r=3.14
+// console.log(r);
+// m=5
+// let v =a3
+// let s=6*a2
+// console.log(s);
+// m6
+// let a=1
+
